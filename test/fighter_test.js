1846e49bb17710f0c31e8450278c4894517790b1
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-06-24 13:24:33 +0200

    correct indent of fighter.js

--- test/fighter_test.js
+++ test/fighter_test.js
@@ -5,19 +5,19 @@ var Fighter = require('../lib/fighter.js');
 
 describe('Fighter', function() {
     it('should exist', function() {
-	expect(Fighter).to.exist;
+        expect(Fighter).to.exist;
     });
 
     it('should be a constructor', function() {
-	expect(Fighter).to.be.a('function');
+        expect(Fighter).to.be.a('function');
     });
 
     it('should be an instance of asteroid', function() {
-	expect(new Fighter()).to.be.a.instanceof(require('asteroids-asteroid'));
+        expect(new Fighter()).to.be.a.instanceof(require('asteroids-asteroid'));
     });
 
     it('should have a function to fire', function() {
-	expect(new Fighter().fire).to.be.a('function');
+        expect(new Fighter().fire).to.be.a('function');
     });
 
 
@@ -27,20 +27,20 @@ describe('Fighter', function() {
 
 describe('initializer', function(){
     it('should call the initializer function', function(done) {
-	var fighter = new Fighter ( function () {
-	    done();
-	});
+        var fighter = new Fighter(function () {
+            done();
+        });
     });
+
     it('should pass the fighter as an argument to the initializer', function() {
-
-	new Fighter ( function (fighter) {
-	    expect(fighter).to.exist;
-	});
+        new Fighter (function(fighter) {
+            expect(fighter).to.exist;
+        });
     });
-    it('should pass the fighter as a context as well as an argument', function() {
 
-	new Fighter ( function (fighter) {
-	    expect(fighter).to.equal(this);
-	});
+    it('should pass the fighter as a context as well as an argument', function() {
+        new Fighter (function (fighter) {
+            expect(fighter).to.equal(this);
+        });
     });
 });
